b96a11eb9b220c4f705a97275db4b5e7b0d1ec22
edvvarrd <edvvardsky@gmail.com>
2022-12-14 03:26:40 +0200

    Last tweaks for mobile nav

--- js/main.js
+++ js/main.js
@@ -1,10 +1,17 @@
 const navMobile = document.querySelector('.nav-mobile')
+const navLink = document.querySelectorAll('.nav__link')
 const navButton = document.querySelector('.hamburger')
 const footerYear = document.querySelector('.footer__year')
 
 const handleNav = () => {
 	navButton.classList.toggle('is-active')
 	navMobile.classList.toggle('nav-mobile--active')
+	navLink.forEach(item => {
+		item.addEventListener('click', () => {
+			navButton.classList.remove('is-active')
+			navMobile.classList.remove('nav-mobile--active')
+		})
+	})
 }
 navButton.addEventListener('click', handleNav)
 
